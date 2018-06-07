cf66606bc09d5fbb8f33b4427fdf190900468537
nadegdaMu <bykovanadegda@gmail.com>
2018-06-07 13:03:19 +0700

    Параметры функции для отрисовки диаграммы вынесены в статичные глобальные перемены, вычисления высот столбиков из отдельных функций перенесены в функцию по отрисовке диаграммы

--- js/stat.js
+++ js/stat.js
@@ -1,38 +1,26 @@
 'use strict';
 
 var GIST_HEIGHT = 150;
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var GIST_X = 200;
+var GIST_Y = 100;
+var WIDTH_COLOMN = 40;
 
-var renderCloud1 = function (ctx) {
-  var bezierCloud = [
-    [80, 40, 150, -10, 250, 40],
-    [250, 40, 350, -10, 450, 40],
-    [450, 40, 530, -10, 600, 40],
-    [600, 40, 650, 80, 600, 150],
-    [600, 150, 650, 250, 580, 280],
-    [580, 280, 550, 300, 450, 280],
-    [450, 280, 370, 300, 300, 280],
-    [300, 280, 250, 300, 100, 280],
-    [100, 280, 80, 280, 100, 300],
-    [100, 300, 40, 200, 80, 200],
-    [80, 200, 0, 100, 80, 40]];
-  ctx.shadowOffsetX = 10;
-  ctx.shadowOffsetY = 10;
-  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillStyle = '#fff';
-  ctx.strokeStyle = '#fff';
-  ctx.beginPath();
-
-  for (var i = 0; i < bezierCloud.length; i++) {
-    var [c1, c2, c3, c4, c5, c6] = bezierCloud[i];
-    ctx.bezierCurveTo(c1, c2, c3, c4, c5, c6);
-  }
-
-  ctx.closePath();
-  ctx.stroke();
-  ctx.fill();
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var calculetGistHeight = function (times) {
+var colorOther = function () {
+  return 'rgba(0, 0, 255, ' + Math.random(250) + ')';
+};
+
+var renderGist = function (ctx, names, times) {
+  var bottomGist = GIST_Y + GIST_HEIGHT;
+  var offset = 20;
+  var stepColomn = 50;
+  var colorU = 'rgba(255, 0, 0, 1)';
   var maxTime = 0;
   var gistHeight = [];
 
@@ -45,43 +33,31 @@ var calculetGistHeight = function (times) {
   for (var j = 0; j < times.length; j++) {
     gistHeight[j] = (times[j] * GIST_HEIGHT) / maxTime;
   }
-  return gistHeight;
-};
 
-var colorOther = function () {
-  return 'rgba(0, 0, 255, ' + Math.random(250) + ')';
-};
-
-var renderGist = function (ctx, names, times, gistdata, gistx, gisty, widthColomn) {
-  var bottomGist = gisty + GIST_HEIGHT;
-  var offset = 20;
-  var stepColomn = 50;
-
-  var colorU = 'rgba(255, 0, 0, 1)';
-
-  for (var i = 0; i < names.length; i++, gistx += stepColomn) {
-    if (names[i] === 'Вы') {
+  for (var k = 0; k < names.length; k++, GIST_X += stepColomn) {
+    if (names[k] === 'Вы') {
       ctx.fillStyle = colorU;
     } else {
       ctx.fillStyle = colorOther();
     }
 
-    ctx.fillRect(gistx, bottomGist, widthColomn, -gistdata[i]);
-    ctx.fillText(names[i], gistx, bottomGist + offset);
-    ctx.fillText(parseInt(times[i]), gistx, bottomGist - gistdata[i] - 5);
+    ctx.fillRect(GIST_X, bottomGist, WIDTH_COLOMN, -gistHeight[k]);
+    ctx.fillText(names[k], GIST_X, bottomGist + offset);
+    ctx.fillText(parseInt(times[k], 10), GIST_X, bottomGist - gistHeight[k] - 5);
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud1(ctx);
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'center';
   ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 80, 60);
-  ctx.fillText('Список результатов:', 80, 80);
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
 
-  renderGist(ctx, names, times, calculetGistHeight(times), 200, 100, 40);
+  renderGist(ctx, names, times);
 };
