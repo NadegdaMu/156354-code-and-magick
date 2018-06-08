c7a5680b6ec4e5bfbd99c35195ab5b45c9576843
nadegdaMu <bykovanadegda@gmail.com>
2018-06-08 09:54:42 +0700

    Изменения позиции и расстояния между колонками

--- js/stat.js
+++ js/stat.js
@@ -3,26 +3,35 @@
 var GIST_HEIGHT = 150;
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
-var GIST_X = 200;
+var GIST_X = 110;
 var GIST_Y = 100;
 var WIDTH_COLOMN = 40;
+
+// функция для отрисовки облака с тенью
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+// функция для определения случайного оттенка синего цвета
+
 var colorOther = function () {
   return 'rgba(0, 0, 255, ' + Math.random(250) + ')';
 };
 
+// функция для отрисивки диаграммы
+
 var renderGist = function (ctx, names, times) {
   var bottomGist = GIST_Y + GIST_HEIGHT;
   var offset = 20;
-  var stepColomn = 50;
+  var stepColomn = WIDTH_COLOMN + 50;
   var colorU = 'rgba(255, 0, 0, 1)';
   var maxTime = 0;
   var gistHeight = [];
+  var paddingGistX = (CLOUD_WIDTH - names.length * stepColomn) / 2; // вычисление начальной точки отрисовки диаграммы
+
+  // цикл для поиска наибольшего времени игры в массиве результатов игры
 
   for (var i = 0; i < times.length; i++) {
     if (times[i] > maxTime) {
@@ -30,22 +39,29 @@ var renderGist = function (ctx, names, times) {
     }
   }
 
+  // цикл для определения высоты каждого столбика времени.
+
   for (var j = 0; j < times.length; j++) {
     gistHeight[j] = (times[j] * GIST_HEIGHT) / maxTime;
   }
 
-  for (var k = 0; k < names.length; k++, GIST_X += stepColomn) {
+  // цикл для отрисовки диаграммы
+
+  for (var k = 0, l = GIST_X + paddingGistX; k < names.length; k++, l += stepColomn) {
+
     if (names[k] === 'Вы') {
       ctx.fillStyle = colorU;
     } else {
       ctx.fillStyle = colorOther();
     }
 
-    ctx.fillRect(GIST_X, bottomGist, WIDTH_COLOMN, -gistHeight[k]);
-    ctx.fillText(names[k], GIST_X, bottomGist + offset);
-    ctx.fillText(parseInt(times[k], 10), GIST_X, bottomGist - gistHeight[k] - 5);
+    ctx.fillRect(l, bottomGist, WIDTH_COLOMN, -gistHeight[k]);
+    ctx.fillText(names[k], l, bottomGist + offset);
+    ctx.fillText(parseInt(times[k], 10), l, bottomGist - gistHeight[k] - 5);
   }
 };
+
+// функция для отрисовки canvas
 
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
